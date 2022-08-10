15d3d47da119cbae23452ec64e1475bab3bc649b
subinJo <sub2nnj5@gmail.com>
2022-08-10 20:18:43 +0900

    complete quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,17 +1,31 @@
 import pandas as pd
 
-data = pd.read_csv('RN.csv')
+def csvtolist():
+    data = pd.read_csv('UN.csv', names=['num'])
+    n = data['num']
+    n_list = n.values.tolist()
+    print(n_list)
 
-def quick_sort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[len(arr) // 2]
-    lesser_arr, equal_arr, greater_arr = [], [], []
-    for num in arr:
+    return n_list
+
+def quick_sort(n_list):
+    if len(n_list) <= 1:
+        return n_list
+    pivot = n_list[len(n_list) // 2]
+    lesser_list, equal_list, greater_list = [], [], []
+    for num in n_list:
         if num < pivot:
-            lesser_arr.append(num)
+            lesser_list.append(num)
         elif num > pivot:
-            greater_arr.append(num)
+            greater_list.append(num)
         else:
-            equal_arr.append(num)
-    return quick_sort(lesser_arr) + equal_arr + quick_sort(greater_arr)
+            equal_list.append(num)
+    return quick_sort(lesser_list) + equal_list + quick_sort(greater_list)
+
+
+if __name__ == "__main__":
+    n_list = csvtolist()
+    quick = quick_sort(n_list)
+
+    print(quick)
+    print(len(quick))
