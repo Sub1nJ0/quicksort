b0bc3abe2c760b14b8ee75dcd9f5063cb38cad13
subinJo <sub2nnj5@gmail.com>
2022-08-10 21:24:22 +0900

    change quick_sorted list to csv file

--- quicksort.py
+++ quicksort.py
@@ -1,31 +1,63 @@
 import pandas as pd
+import time
+import psutil
+import csv
 
 def csvtolist():
-    data = pd.read_csv('UN.csv', names=['num'])
+    data = pd.read_csv('RN.csv', names=['num'])
     n = data['num']
     n_list = n.values.tolist()
     print(n_list)
 
     return n_list
 
-def quick_sort(n_list):
+def writeFile(quick):
+    f = open('Result.csv', 'w', newline='')
+    wr = csv.writer(f)
+    for i in quick:
+        list = [i]
+        wr.writerow(list)
+    f.close()
+
+
+def quicksort(n_list):
     if len(n_list) <= 1:
         return n_list
+
     pivot = n_list[len(n_list) // 2]
-    lesser_list, equal_list, greater_list = [], [], []
+    left, mid, right = [], [], []
+
     for num in n_list:
         if num < pivot:
-            lesser_list.append(num)
+            left.append(num)
         elif num > pivot:
-            greater_list.append(num)
+            right.append(num)
         else:
-            equal_list.append(num)
-    return quick_sort(lesser_list) + equal_list + quick_sort(greater_list)
+            mid.append(num)
+    return quicksort(left) + mid + quicksort(right)
 
 
 if __name__ == "__main__":
     n_list = csvtolist()
-    quick = quick_sort(n_list)
+
+    start_p = psutil.Process()
+    start_rss = start_p.memory_info().rss / 2 ** 20  # bytes to mb
+    start_time = time.time()
+
+    quick = quicksort(n_list)
+
+    end_time = time.time()
+    end_p = psutil.Process()
+    end_rss = end_p.memory_info().rss / 2 ** 20  # bytes to mb
+
+    t = end_time - start_time
+    m = end_rss - start_rss
 
     print(quick)
     print(len(quick))
+    print(f"실행 시간: {t}")
+    print(f"사용 메모리: {m}MB")
+    #print(start_p.memory_info())
+    #print(end_p.memory_info())
+
+    writeFile(quick)
